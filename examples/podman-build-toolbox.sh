e5333b9fb7c19fea62ea2a9c2bfc3764b1f9162f
David O. <david@oszi.dev>
2024-06-08 00:12:00 +0200

    Remove obsolete is_container fact

--- examples/podman-build-toolbox.sh
+++ examples/podman-build-toolbox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd "$(dirname "$(realpath "$0")")"
 set -euo pipefail
+cd "$(dirname "$(realpath "$0")")"
 
 base_image="registry.fedoraproject.org/fedora:latest"
 image="toolbox:latest"
@@ -11,8 +11,7 @@ podman create -t --name="$container" "$base_image" bash
 trap "podman rm -ifv "$container"" EXIT
 podman start "$container"
 
-ansible-playbook -i "${container}," -c podman -e is_container=true \
-  oszi.environments.toolbox
+ansible-playbook -c podman -i "${container}," oszi.environments.toolbox
 
 podman exec "$container" sh -c "rm -rf /root/.cache/* /var/cache/ansible/* /var/cache/dnf/*"
 podman commit -c "LABEL com.github.containers.toolbox=\"true\"" "$container" "$image"
