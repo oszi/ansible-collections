99c5bc1130c3b2052de7ce4f989ae46751834ff6
David O. <david@oszi.dev>
2024-11-13 19:55:55 +0100

    Update podman-build-toolbox.sh

--- examples/podman-build-toolbox.sh
+++ examples/podman-build-toolbox.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 cd -- "$(dirname -- "$(realpath -- "$0")")"
 
-base_image="registry.fedoraproject.org/fedora:latest"
+base_image="quay.io/fedora/fedora:latest"
 image="quay.io/oszi/toolbox:latest"
 container="toolbox-${RANDOM}"
 set -x
@@ -10,8 +10,9 @@ set -x
 podman create -t --name="$container" "$base_image" bash
 trap 'podman rm -ifv "$container"' EXIT
 podman start "$container"
+podman exec "$container" dnf -y install python3-libdnf5  # Fedora 41+
 
 ansible-playbook -c podman -i "${container}," oszi.environments.toolbox
 
 podman exec "$container" sh -c "rm -rf /root/.cache/* /var/cache/ansible/* /var/cache/dnf/*"
-podman commit -c "LABEL com.github.containers.toolbox=\"true\"" "$container" "$image"
+podman commit -c 'LABEL com.github.containers.toolbox="true"' "$container" "$image"
