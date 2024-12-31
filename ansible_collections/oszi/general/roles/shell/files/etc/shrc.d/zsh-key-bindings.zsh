6d571cd7815aaf24485bfd7574ffed097e53cfd1
David Osztertag <david@oszi.dev>
2024-12-31 14:57:08 +0100

    Update zsh-key-bindings comments

--- ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/zsh-key-bindings.zsh
+++ ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/zsh-key-bindings.zsh
@@ -18,17 +18,17 @@ autoload -Uz down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey '^?'      backward-delete-char           # BS          Delete one char backward
-bindkey '^[[3~'   delete-char                    # Delete      Delete one char forward
-bindkey '^[[H'    beginning-of-line              # Home        Go to the beginning of line
-bindkey '^[[F'    end-of-line                    # End         Go to the end of line
-bindkey '^[[1;5C' forward-word                   # Ctrl+Right  Go forward one word
-bindkey '^[[1;5D' backward-word                  # Ctrl+Left   Go backward one word
-bindkey '^H'      backward-kill-word             # Ctrl+BS     Delete previous word
-bindkey '^[[3;5~' kill-word                      # Ctrl+Del    Delete next word
-bindkey '^[[D'    backward-char                  # Left        Move cursor one char backward
-bindkey '^[[C'    forward-char                   # Right       Move cursor one char forward
-bindkey '^[[A'    up-line-or-beginning-search    # Up          Prev command in history
-bindkey '^[[B'    down-line-or-beginning-search  # Down        Next command in history
-bindkey '^[[5~'   history-search-backward        # Page Up     Prev command in history
-bindkey '^[[6~'   history-search-forward         # Page Down   Next command in history
+bindkey '^?'      backward-delete-char           # Backspace
+bindkey '^[[3~'   delete-char                    # Delete
+bindkey '^[[H'    beginning-of-line              # Home
+bindkey '^[[F'    end-of-line                    # End
+bindkey '^[[1;5C' forward-word                   # Ctrl+Right
+bindkey '^[[1;5D' backward-word                  # Ctrl+Left
+bindkey '^H'      backward-kill-word             # Ctrl+Backspace
+bindkey '^[[3;5~' kill-word                      # Ctrl+Del
+bindkey '^[[D'    backward-char                  # Left
+bindkey '^[[C'    forward-char                   # Right
+bindkey '^[[A'    up-line-or-beginning-search    # Up
+bindkey '^[[B'    down-line-or-beginning-search  # Down
+bindkey '^[[5~'   history-search-backward        # Page Up
+bindkey '^[[6~'   history-search-forward         # Page Down
