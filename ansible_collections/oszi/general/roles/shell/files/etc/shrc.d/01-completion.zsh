50e312e5dd945e2cde44e9297ed0a74803d380cc
David Osztertag <david@oszi.dev>
2025-01-06 12:21:12 +0100

    Update completion for ZSH

--- ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-completion.zsh
+++ ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-completion.zsh
@@ -1,6 +1,16 @@
-# shellcheck shell=sh
+# shellcheck shell=sh disable=SC2016 # single quotes
 
 autoload -Uz +X compinit && compinit
 autoload -Uz +X bashcompinit && bashcompinit
 
+setopt COMPLETE_IN_WORD
+setopt ALWAYS_TO_END
+
 zstyle ':completion:*' completer _complete _ignored _files
+zstyle ':completion:*' verbose yes
+
+zstyle ':completion:*:kill:*' command 'ps -u $UID -o pid=,tty=,cmd='
+zstyle ':completion:*:kill:*' menu yes select
+
+zstyle ':completion:*:killall:*' command 'ps -u $UID -o comm='
+zstyle ':completion:*:killall:*' menu yes select
