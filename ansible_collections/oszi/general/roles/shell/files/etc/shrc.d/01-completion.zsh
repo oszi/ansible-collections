1a72e269ec8bfa6248e4c0c8b176c9ca89ba4524
David O. <david@oszi.dev>
2025-01-09 20:35:25 +0100

    Improve ZSH completion

--- ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-completion.zsh
+++ ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-completion.zsh
@@ -1,16 +1,27 @@
-# shellcheck shell=sh disable=SC2016 # single quotes
+# shellcheck shell=bash disable=SC2296 # zsh is unsupported
+# shellcheck disable=SC2016,SC2034 # single quotes, variable appears unused
 
 autoload -Uz +X compinit && compinit
 autoload -Uz +X bashcompinit && bashcompinit
 
 setopt COMPLETE_IN_WORD
-setopt ALWAYS_TO_END
 
-zstyle ':completion:*' completer _complete _ignored _files
+_complete_reply() {
+    if [[ "$_complete_last_try" != "${HISTNO}${BUFFER}${CURSOR}" ]]; then
+        _complete_last_try="${HISTNO}${BUFFER}${CURSOR}"
+        reply=(_complete _match _prefix _files)
+    else
+        reply=(_complete _ignored _correct _approximate _files)
+    fi
+}
+
+zstyle -e ':completion:*' completer _complete_reply
 zstyle ':completion:*' verbose yes
 
-zstyle ':completion:*:kill:*' command 'ps -u $UID -o pid=,tty=,cmd='
-zstyle ':completion:*:kill:*' menu yes select
+zstyle ':completion:*:correct:*' insert-unambiguous true
+zstyle ':completion:*:correct:*' original true
 
-zstyle ':completion:*:killall:*' command 'ps -u $UID -o comm='
-zstyle ':completion:*:killall:*' menu yes select
+zstyle ':completion:*:*:kill:*' command 'ps -u $UID -o pid=,tty=,cmd='
+zstyle ':completion:*:*:kill:*' menu yes select
+
+zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"
