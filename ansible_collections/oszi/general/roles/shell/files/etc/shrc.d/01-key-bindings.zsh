4ca5a491a7f3952afa5cf23521eb488347d8ceea
David O. <david@oszi.dev>
2025-01-13 18:11:24 +0100

    Improve ZSH key-bindings
    Based on Debian/Ubuntu /etc/zsh/zshrc

--- ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-key-bindings.zsh
+++ ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-key-bindings.zsh
@@ -1,4 +1,60 @@
-# shellcheck shell=sh disable=SC1091 # not following input sources
+# shellcheck shell=bash disable=SC2190 # zsh is unsupported
+# shellcheck disable=SC1091,SC2154 # not following input sources, terminfo not assigned
+
+# Make sure that the terminal is in application mode when zle is active,
+# since only then values from $terminfo are valid.
+if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
+    function zle-line-init() {
+        printf '%s' "${terminfo[smkx]}"
+    }
+
+    function zle-line-finish() {
+        printf '%s' "${terminfo[rmkx]}"
+    }
+
+    zle -N zle-line-init
+    zle -N zle-line-finish
+fi
+
+typeset -g -A key
+key=(
+    Backspace        "${terminfo[kbs]}"   # ^?
+    Insert           "${terminfo[kich1]}" # ^[[2~
+    Delete           "${terminfo[kdch1]}" # ^[[3~
+    Home             "${terminfo[khome]}" # ^[[H
+    End              "${terminfo[kend]}"  # ^[[F
+    PageUp           "${terminfo[kpp]}"   # ^[[5~
+    PageDown         "${terminfo[knp]}"   # ^[[6~
+    ShiftTab         "${terminfo[kcbt]}"  # ^[[Z
+    Up               "${terminfo[kcuu1]}" # ^[[A
+    Down             "${terminfo[kcud1]}" # ^[[B
+    Left             "${terminfo[kcub1]}" # ^[[D
+    Right            "${terminfo[kcuf1]}" # ^[[C
+    ControlLeft      "${terminfo[kLFT5]}" # ^[[1;5D
+    ControlRight     "${terminfo[kRIT5]}" # ^[[1;5C
+    ControlBackspace '^H'
+    ControlDelete    '^[[3;5~'
+)
+
+function bind2maps() {
+    local i sequence widget
+    local -a maps
+
+    while [[ "$1" != "--" ]]; do
+        maps+=("$1")
+        shift
+    done
+    shift
+
+    sequence="${key[$1]}"
+    widget="$2"
+
+    [[ -z "$sequence" ]] && return 1
+
+    for i in "${maps[@]}"; do
+        bindkey -M "$i" "$sequence" "$widget"
+    done
+}
 
 if [ -f /usr/share/fzf/shell/key-bindings.zsh ]; then
     . /usr/share/fzf/shell/key-bindings.zsh
@@ -6,35 +62,41 @@ elif [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ]; then
     . /usr/share/doc/fzf/examples/key-bindings.zsh
 fi
 
-# If the terminal is in application mode (smkx), translate keys
-# to make them appear the same as in raw mode (rmkx).
-bindkey -s '^[OH' '^[[H'  # Home
-bindkey -s '^[OF' '^[[F'  # End
-bindkey -s '^[OA' '^[[A'  # Up
-bindkey -s '^[OB' '^[[B'  # Down
-bindkey -s '^[OD' '^[[D'  # Left
-bindkey -s '^[OC' '^[[C'  # Right
-
-# TTY sends different key codes, translate them to regular.
-bindkey -s '^[[1~' '^[[H'  # Home
-bindkey -s '^[[4~' '^[[F'  # End
-
 autoload -Uz up-line-or-beginning-search
 autoload -Uz down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
 
-bindkey '^?'      backward-delete-char           # Backspace
-bindkey '^[[3~'   delete-char                    # Delete
-bindkey '^[[H'    beginning-of-line              # Home
-bindkey '^[[F'    end-of-line                    # End
-bindkey '^[[1;5C' forward-word                   # Ctrl+Right
-bindkey '^[[1;5D' backward-word                  # Ctrl+Left
-bindkey '^H'      backward-kill-word             # Ctrl+Backspace
-bindkey '^[[3;5~' kill-word                      # Ctrl+Del
-bindkey '^[[D'    backward-char                  # Left
-bindkey '^[[C'    forward-char                   # Right
-bindkey '^[[A'    up-line-or-beginning-search    # Up
-bindkey '^[[B'    down-line-or-beginning-search  # Down
-bindkey '^[[5~'   history-search-backward        # Page Up
-bindkey '^[[6~'   history-search-forward         # Page Down
+bind2maps emacs             -- Backspace        backward-delete-char
+bind2maps       viins       -- Backspace        vi-backward-delete-char
+bind2maps             vicmd -- Backspace        vi-backward-char
+bind2maps emacs viins       -- Insert           overwrite-mode
+bind2maps             vicmd -- Insert           vi-insert
+bind2maps emacs             -- Delete           delete-char
+bind2maps       viins vicmd -- Delete           vi-delete-char
+bind2maps emacs             -- Home             beginning-of-line
+bind2maps       viins vicmd -- Home             vi-beginning-of-line
+bind2maps emacs             -- End              end-of-line
+bind2maps       viins vicmd -- End              vi-end-of-line
+bind2maps emacs viins vicmd -- PageUp           up-line-or-history
+bind2maps emacs viins vicmd -- PageDown         down-line-or-history
+bind2maps emacs viins vicmd -- ShiftTab         reverse-menu-complete
+bind2maps emacs viins vicmd -- Up               up-line-or-beginning-search
+bind2maps emacs viins vicmd -- Down             down-line-or-beginning-search
+bind2maps emacs             -- Left             backward-char
+bind2maps       viins vicmd -- Left             vi-backward-char
+bind2maps emacs             -- Right            forward-char
+bind2maps       viins vicmd -- Right            vi-forward-char
+bind2maps emacs             -- ControlLeft      backward-word
+bind2maps       viins vicmd -- ControlLeft      vi-backward-word
+bind2maps emacs             -- ControlRight     forward-word
+bind2maps       viins vicmd -- ControlRight     vi-forward-word
+bind2maps emacs             -- ControlBackspace backward-kill-word
+bind2maps       viins vicmd -- ControlBackspace vi-backward-kill-word
+bind2maps emacs viins vicmd -- ControlDelete    kill-word
+
+bindkey '\ew' kill-region          # [Esc+w]
+bindkey '\em' copy-prev-shell-word # [Esc+m]
+
+unset -f bind2maps
+unset key
