1b4f0a99f4639c4afdb4230cb9de420d6df28637
David O. <david@oszi.dev>
2025-01-14 18:08:00 +0100

    Simplify ZSH completion methods

--- ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-completion.zsh
+++ ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-completion.zsh
@@ -6,20 +6,10 @@ autoload -Uz +X bashcompinit && bashcompinit
 
 setopt COMPLETE_IN_WORD
 
-_complete_reply() {
-    if [[ "$_complete_last_try" != "${HISTNO}${BUFFER}${CURSOR}" ]]; then
-        _complete_last_try="${HISTNO}${BUFFER}${CURSOR}"
-        reply=(_complete _match _prefix _files)
-    else
-        reply=(_complete _ignored _correct _approximate _files)
-    fi
-}
-
-zstyle -e ':completion:*' completer _complete_reply
+zstyle ':completion:*' completer _complete _match _prefix _approximate _files
 zstyle ':completion:*' verbose yes
 
-zstyle ':completion:*:correct:*' insert-unambiguous true
-zstyle ':completion:*:correct:*' original true
+zstyle ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3)) numeric)'
 
 zstyle ':completion:*:*:kill:*' command 'ps -u $UID -o pid=,tty=,cmd='
 zstyle ':completion:*:*:kill:*' menu yes select
