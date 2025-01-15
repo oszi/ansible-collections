a0042485b736f185b4244070c326183a59c3800c
David O. <david@oszi.dev>
2025-01-15 22:10:55 +0100

    Revert "Magic bash completion fallback for ZSH"
    This reverts commit 5e9c529318b835a7850197d3f3312b778c935126.

--- ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-completion.zsh
+++ ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/01-completion.zsh
@@ -1,6 +1,5 @@
-# shellcheck shell=bash disable=SC2296,SC2298 # zsh is unsupported
+# shellcheck shell=bash disable=SC2296 # zsh is unsupported
 # shellcheck disable=SC2016,SC2034 # single quotes, variable appears unused
-# shellcheck disable=SC1090 # non-constant source
 
 autoload -Uz +X compinit && compinit
 autoload -Uz +X bashcompinit && bashcompinit
@@ -16,33 +15,3 @@ zstyle ':completion:*:*:kill:*' command 'ps -u $UID -o pid=,tty=,cmd='
 zstyle ':completion:*:*:kill:*' menu yes select
 
 zstyle ':completion:*:default' list-colors "${(s.:.)LS_COLORS}"
-
-# Try loading bash completions for commands that have no native completion.
-# See the key-binding below. Your mileage may vary.
-
-typeset -g -r -a _BASH_COMPLETIONS_PATH=(/etc/bash_completion.d /usr/share/bash-completion/completions)
-typeset -g -A _bash_completions_tried
-
-function _bash_complete_fallback() {
-    local BASH_SOURCE
-    local _cmd=${${(zA)BUFFER}[1]:t}
-
-    if [[ $_cmd && ! ${_bash_completions_tried[$_cmd]} ]]; then
-        rehash
-        if ! whence "_${_cmd}" >/dev/null 2>&1; then
-            for BASH_SOURCE in ${^_BASH_COMPLETIONS_PATH}/${_cmd}; do
-                if [[ -r "$BASH_SOURCE" ]]; then
-                    emulate ksh -c '. "$BASH_SOURCE"'
-                    zle reset-prompt
-                    break
-                fi
-            done
-        fi
-        _bash_completions_tried[$_cmd]=1
-    fi
-
-    zle complete-word
-}
-
-zle -N bash-complete-fallback _bash_complete_fallback
-bindkey '\e\t' bash-complete-fallback # [Esc+Tab]
