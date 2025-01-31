7ae13d4fd9d4bab0618715af91a1b5cf2990c604
David O. <david@oszi.dev>
2025-01-31 17:19:17 +0100

    Use git-ls-files in git-pre-commit.sh

--- _scripts/git-pre-commit.sh
+++ _scripts/git-pre-commit.sh
@@ -14,12 +14,20 @@ COLOR_RED="\033[31m"
 COLOR_GREEN="\033[32m"
 COLOR_YELLOW="\033[33m"
 
-VAULT_FILES_REGEX='.*/\(group\|host\)_vars/.*vault[^/]*\.\(ya?ml\|json\)$'
-KEY_FILES_REGEX='.*\(\(private\|privkey\|-key\)\.pem\|\.key\|\.csr\|\.p12\)$'
+if ! [[ "${1-}" =~ ^-*(y|yes|f|force)$ ]]; then
+    echo -en "${COLOR_YELLOW}Run ansible-lint and shellcheck?${COLOR_CLEAR} [y/N]" >&2
+    read -r answer
+    if ! [[ "$answer" =~ ^[Yy] ]]; then
+        exit 0
+    fi
+fi
 
-# shellcheck disable=SC2016 # quoting '^$ANSIBLE_VAULT'
-vault_files="$(find . -type f \( -iregex "$VAULT_FILES_REGEX" -or -iregex "$KEY_FILES_REGEX" \) -print0 \
-    | xargs -0 -r grep --files-without-match -- '^$ANSIBLE_VAULT')" ||:
+GIT_LS_FILES='git ls-files -c -o --exclude-standard --deduplicate'
+
+vault_files=$(
+    ${GIT_LS_FILES} -z -- '**_vars/**vault.'{yaml,yml,json} '*.'{key,csr,p12} '**/'{private,privkey,\*-key}.pem \
+        | xargs -0 -r grep --files-without-match -- $'^$ANSIBLE_VAULT'
+) ||:
 
 if [[ "$vault_files" != "" ]]; then
     echo -e "${COLOR_RED}Clear-text private keys or vault files!${COLOR_CLEAR}" >&2
@@ -27,16 +35,12 @@ if [[ "$vault_files" != "" ]]; then
     exit 1
 fi
 
-SHELL_FILES_REGEX='.*\(\.\(sh\|bash\|ksh\|zsh\)\|[/.]\w*\(shrc\|profile\)\)$'
-SHELL_PATHS_REGEX='.*/\(_*scripts\|s?bin\)/.+$'
-AWK_SHEBANG_MATCH='FNR>1 {nextfile} /^#![^ ]+[/ ](sh|bash|ksh|zsh)$/ {print FILENAME; nextfile}'
-
 shellcheck-targets() {
-    find . -type f -iregex "$SHELL_FILES_REGEX"
+    local IFS=' '
 
-    find . -type f -iregex "$SHELL_PATHS_REGEX" \
-        -not -iregex "$SHELL_FILES_REGEX" -not -iregex '.*\.\(j2\|jinja\)$' -print0 \
-        | xargs -0 -r awk -- "$AWK_SHEBANG_MATCH"
+    ${GIT_LS_FILES} -- '*.'{sh,bash,ksh,zsh} '**/*shrc' '**/.*profile'
+    ${GIT_LS_FILES} -z -- '**scripts/*' '**/bin/*' '**/sbin/*' ':!:*.'{j2,jinja} \
+        | xargs -0 -r awk -- 'FNR>1 {nextfile} /^#![^ ]+[/ ](sh|bash|ksh|zsh)$/ {print FILENAME; nextfile}'
 }
 
 shellcheck-lint() {
@@ -55,8 +59,4 @@ shellcheck-lint() {
     fi
 }
 
-echo -en "${COLOR_YELLOW}Run ansible-lint and shellcheck?${COLOR_CLEAR} [y/N]" >&2
-read -r answer
-if [[ "$answer" =~ ^[Yy] ]]; then
-    ansible-lint && shellcheck-lint
-fi
+ansible-lint && shellcheck-lint
