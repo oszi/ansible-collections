2a1dada7e1292697c244bd761453cb98abba284f
David O. <david@oszi.dev>
2025-02-11 09:20:28 +0100

    Verify HEAD in git-init.sh

--- _scripts/git-init.sh
+++ _scripts/git-init.sh
@@ -4,8 +4,11 @@
 set -euo pipefail
 cd -- "$(git rev-parse --show-toplevel)"
 
+git log --oneline HEAD^..HEAD --
+git verify-commit HEAD
+
+git submodule update --init --checkout --recursive
+
 if [ -e _scripts/git-pre-commit.sh ]; then
     ln -sfv ../../_scripts/git-pre-commit.sh .git/hooks/pre-commit
 fi
-
-git submodule update --init --checkout --recursive
