14bff5552cbe90d9fe95d24e9b786925c44b9b25
David Osztertag <david@oszi.dev>
2025-02-26 10:40:00 +0100

    Improve hooks in git-init.sh

--- _scripts/git-init.sh
+++ _scripts/git-init.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
 # Script to initialize the git repository.
-# You may COPY this file into a parent repository - e.g. inventory.
+# You may call this script in a parent repository, like so:
+
+##!/bin/bash
+#set -euo pipefail
+#cd -- "$(git rev-parse --show-toplevel)"
+#git submodule update --init --checkout --recursive
+#collections/_scripts/git-init.sh
+
 set -euo pipefail
 cd -- "$(git rev-parse --show-toplevel)"
-
 git submodule update --init --checkout --recursive
 
-if [[ -r _scripts/git-pre-commit.sh ]]; then
-    ln -sfv ../../_scripts/git-pre-commit.sh .git/hooks/pre-commit
-fi
+hooks_path="$(git rev-parse --git-dir)/hooks"
+hooks=(
+    pre-commit
+    post-commit
+    pre-merge-commit
+    pre-rebase
+    post-checkout
+    pre-receive
+    post-receive
+    pre-push
+)
+
+setup_hook() {
+    local hook="$1"
+    local hook_path="_scripts/git-${hook}.sh"
+
+    if [[ -x "$hook_path" ]]; then
+        hook_path="$(realpath -s --relative-to="$hooks_path" "$hook_path")"
+        ln -sfv "$hook_path" "${hooks_path}/${hook}"
+    fi
+}
+
+for hook in "${hooks[@]}"; do
+    setup_hook "$hook"
+done
