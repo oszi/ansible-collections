ca7419157d4dd5e9e107f59cf34c4588d9fa11a5
David O. <david@oszi.dev>
2025-02-28 12:26:56 +0100

    Add color feedback to git-verify.sh

--- _scripts/git-verify.sh
+++ _scripts/git-verify.sh
@@ -7,6 +7,10 @@ cd -- "$(git rev-parse --show-toplevel)"
 GPG_KEY_ID="AFDE0AB3943D1FB3"
 GPG_IMPORT_URL="https://oszi.dev/oszi.dev.asc"
 
+COLOR_CLEAR="\033[0m"
+COLOR_RED="\033[31m"
+COLOR_GREEN="\033[32m"
+
 if ! gpg -k "$GPG_KEY_ID"; then
     echo -en "Import GPG key: ${GPG_KEY_ID} from ${GPG_IMPORT_URL}? [y/N]" >&2
     read -r answer
@@ -16,7 +20,13 @@ if ! gpg -k "$GPG_KEY_ID"; then
 fi
 
 xargs_git() {
-    xargs -r -d'\n' git "$@" -v --
+    if xargs -r -d'\n' git "$@" -v --; then
+        echo -e "${COLOR_GREEN}git $* succeeded${COLOR_CLEAR}" >&2
+        return 0
+    else
+        echo -e "${COLOR_RED}git $* failed${COLOR_CLEAR}" >&2
+        return 1
+    fi
 }
 
 git branch -a --format='%(refname)' | grep -Ev '^\(' | xargs_git verify-commit
