a85abd7e6fa453c0774ec4e8aaf3527c28466d40
David O. <david@oszi.dev>
2025-10-31 08:00:28 +0100

    Add example makefile

---
+++ examples/ansible.mk
@@ -0,0 +1,24 @@
+SHELL := /bin/bash
+
+CHECK     ?=
+LIMIT     ?=
+TAGS      ?=
+SKIP_TAGS ?=
+
+ANSIBLE_PLAYBOOK ?= . ./activate && ansible-playbook
+ANSIBLE_ARGS = $(if $(CHECK), --check --diff,)$(if $(LIMIT), --limit "$(LIMIT)",)$(if $(TAGS), --tags "$(TAGS)",)$(if $(SKIP_TAGS), --skip-tags "$(SKIP_TAGS)",)
+
+localhost: export ANSIBLE_BECOME_ASK_PASS := True
+localhost: ANSIBLE_PLAYBOOK := $(ANSIBLE_PLAYBOOK) --connection local --limit "$$(hostname -f)"
+localhost: workstation
+
+baselinux containerhost toolbox workstation: FORCE
+	$(ANSIBLE_PLAYBOOK) $(ANSIBLE_ARGS) oszi.environments.$@
+
+update versions: FORCE
+	$(ANSIBLE_PLAYBOOK) $(ANSIBLE_ARGS) oszi.general.$@
+
+oszi.% playbooks/%: FORCE
+	$(ANSIBLE_PLAYBOOK) $(ANSIBLE_ARGS) $@
+
+.PHONY: FORCE
