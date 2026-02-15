8472457b45fb149ddec443db724647e19f931653
David O. <david@oszi.dev>
2026-02-15 18:42:00 +0100

    Fix ls colors in shrc.d/00-colors.sh

--- ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/00-colors.sh
+++ ansible_collections/oszi/general/roles/shell/files/etc/shrc.d/00-colors.sh
@@ -5,16 +5,15 @@
 if [ -z "${LS_COLORS-}" ] && command -v dircolors >/dev/null 2>&1; then
     # shellcheck disable=SC2015 # if-then-else
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
 fi
 
-for grep in grep egrep fgrep xzgrep xzegrep xzfgrep zgrep zegrep zfgrep; do
-    if ! alias "$grep" >/dev/null 2>&1 && command -v "$grep" >/dev/null 2>&1; then
+for i in ls grep egrep fgrep xzgrep xzegrep xzfgrep zgrep zegrep zfgrep; do
+    if ! alias "$i" >/dev/null 2>&1 && command -v "$i" >/dev/null 2>&1; then
         # shellcheck disable=SC2139 # expand when defined
-        alias "${grep}=${grep} --color=auto"
+        alias "${i}=${i} --color=auto"
     fi
 done
-unset grep
+unset i
 
 if [ -n "${ZSH_VERSION-}" ]; then
     autoload -Uz +X colors && colors
