633c9ab2bc3981a2dc40c04f09e47154b96f874d
David O. <david@oszi.dev>
2026-04-09 11:47:47 +0200

    Refactor _scripts/_ansible.mk

--- _scripts/_ansible.mk
+++ _scripts/_ansible.mk
@@ -1,23 +1,24 @@
 # Requires ./activate
 
-CHECK     ?=
-LIMIT     ?=
-TAGS      ?=
-SKIP_TAGS ?=
+VERBOSE    ?=
+CHECK      ?=
+CONNECTION ?=
+LIMIT      ?=
+TAGS       ?=
+SKIP_TAGS  ?=
 
 ANSIBLE_PLAYBOOK ?= . ./activate && ansible-playbook
-ANSIBLE_ARGS = $(if $(CHECK), --check --diff,)$(if $(LIMIT), --limit "$(LIMIT)",)$(if $(TAGS), --tags "$(TAGS)",)$(if $(SKIP_TAGS), --skip-tags "$(SKIP_TAGS)",)
+ANSIBLE_ARGS = $(if $(VERBOSE), -vvv,)$(if $(CHECK), --check --diff,)$(if $(CONNECTION), --connection "$(CONNECTION)",)$(if $(LIMIT), --limit "$(LIMIT)",)$(if $(TAGS), --tags "$(TAGS)",)$(if $(SKIP_TAGS), --skip-tags "$(SKIP_TAGS)",)
 
-localhost: ANSIBLE_PLAYBOOK := $(ANSIBLE_PLAYBOOK) --connection local --limit "$$(hostname -f)"
+localhost: LIMIT := $(shell hostname -f)
+localhost: CONNECTION := local
 localhost: workstation
 
-baselinux containerhost toolbox workstation rootless: FORCE
-	$(ANSIBLE_PLAYBOOK) $(ANSIBLE_ARGS) oszi.environments.$@
+baselinux containerhost toolbox workstation rootless: %: oszi.environments.%
 
-update versions: FORCE
-	$(ANSIBLE_PLAYBOOK) $(ANSIBLE_ARGS) oszi.general.$@
+update versions: %: oszi.general.%
 
-oszi.% playbooks/%: FORCE
+playbooks/% oszi.environments.% oszi.general.% oszi.thirdparty.% oszi.utils.%: FORCE
 	$(ANSIBLE_PLAYBOOK) $(ANSIBLE_ARGS) $@
 
 .PHONY: FORCE
