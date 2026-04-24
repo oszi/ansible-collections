ed83b9afcfce98800f6090072ff6c0d5945f78bc
David O. <david@oszi.dev>
2026-04-24 10:49:19 +0200

    Check ./activate and ansible.cfg in _scripts/_ansible.mk

--- _scripts/_ansible.mk
+++ _scripts/_ansible.mk
@@ -7,7 +7,7 @@ LIMIT      ?=
 TAGS       ?=
 SKIP_TAGS  ?=
 
-ANSIBLE_PLAYBOOK ?= . ./activate && ansible-playbook
+ANSIBLE_PLAYBOOK := . ./activate && ansible-playbook
 ANSIBLE_ARGS = $(if $(VERBOSE), -vvv,)$(if $(CHECK), --check --diff,)$(if $(CONNECTION), --connection "$(CONNECTION)",)$(if $(LIMIT), --limit "$(LIMIT)",)$(if $(TAGS), --tags "$(TAGS)",)$(if $(SKIP_TAGS), --skip-tags "$(SKIP_TAGS)",)
 
 localhost: LIMIT := $(shell hostname -f)
@@ -20,7 +20,10 @@ update: %: oszi.general.%
 
 debug versions: %: oszi.utils.%
 
-playbooks/% oszi.environments.% oszi.general.% oszi.thirdparty.% oszi.utils.%: FORCE
+playbooks/% oszi.environments.% oszi.general.% oszi.thirdparty.% oszi.utils.%: activate ansible.cfg FORCE
 	$(ANSIBLE_PLAYBOOK) $(ANSIBLE_ARGS) $@
+
+activate ansible.cfg:
+	@test -f $@ || (echo "./$@ not found! Copy examples/$@"; exit 127)
 
 .PHONY: FORCE
