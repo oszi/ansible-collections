a73209d4297defaad094f1e58d00926f06551ef1
David O. <david@oszi.dev>
2026-09-17 11:33:56 +0200

    Implement shell quoting in _ansible.mk

--- _scripts/_ansible.mk
+++ _scripts/_ansible.mk
@@ -1,4 +1,5 @@
 # Requires ./activate and ./ansible.cfg
+ANSIBLE_PLAYBOOK := . ./activate && ansible-playbook
 
 VERBOSE    ?=
 CHECK      ?=
@@ -7,8 +8,10 @@ LIMIT      ?=
 TAGS       ?=
 SKIP_TAGS  ?=
 
-ANSIBLE_PLAYBOOK := . ./activate && ansible-playbook
-ANSIBLE_ARGS = $(if $(VERBOSE), -vvv,)$(if $(CHECK), --check --diff,)$(if $(CONNECTION), --connection "$(CONNECTION)",)$(if $(LIMIT), --limit "$(LIMIT)",)$(if $(TAGS), --tags "$(TAGS)",)$(if $(SKIP_TAGS), --skip-tags "$(SKIP_TAGS)",)
+_CMD_FLAG = $(if $(filter 1 Y y Yes YES yes True TRUE true,$(value $(2))), $(1),)
+_CMD_ARG  = $(if $(value $(2)), $(1) '$(subst ','\'',$(value $(2)))',)
+
+ANSIBLE_ARGS += $(call _CMD_FLAG,-vvv,VERBOSE)$(call _CMD_FLAG,--check --diff,CHECK)$(call _CMD_ARG,--connection,CONNECTION)$(call _CMD_ARG,--limit,LIMIT)$(call _CMD_ARG,--tags,TAGS)$(call _CMD_ARG,--skip-tags,SKIP_TAGS)
 
 localhost: LIMIT := $(shell hostname -f)
 localhost: CONNECTION := local
